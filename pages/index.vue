30a2f7846235a795ddffe5db77b6c2f3527ffe01
AP-Porag <smauhp@gmail.com>
2021-04-07 02:17:04 +0600

    Second Commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="main-body">
+    <Hero/>
+    <LargeCardContainer
+      v-for="largeCard in largeCards" :key="largeCard.id"
+      :cardContainer="largeCard"
+    />
+    <SmallCardContainer
+    v-for="smallCard in smallCards" :key="smallCard.id"
+    :smallCardContainer="smallCard"
+    />
+  </div>
+</template>
+
+<script>
+import {largeCardSections,smallCardSections} from "@/assets/data.js"
+export default {
+  data(){
+    return{
+      largeCards:largeCardSections,
+      smallCards:smallCardSections,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
